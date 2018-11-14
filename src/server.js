180eca3395fa95fe1cd640798a41f20ec9b5cb97
Gaston Nicolas Perez <gperez@sofrecom.com.ar>
2018-11-14 14:27:06 -0300

    cors en server

--- src/server.js
+++ src/server.js
@@ -12,11 +12,18 @@ var express = require('express'),
   Task = require('./server/models/date'),
   Task = require('./server/models/availableDate'),
 
+  cors = require('cors'),
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true }); 
+mongoose.connect('mongodb://localhost:27017/zenmed', { useNewUrlParser: true }); 
+
+var corsOptions = {
+  origin: ['http://localhost:8099'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
